c5d1b2a8553f5a6339ecce91d8a612216c792d5d
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2020-06-09 21:24:53 +0600

    todo project done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Todo App" />
+    <i @click="toogleAddTodo" class="fas fa-plus"></i>
+    <div v-if="addTodo" class="addTodo">
+      <AddTodo v-on:addTodoEvent="addTodoMethod($event)" />
+    </div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <Todo
+        :data="todo"
+        v-on:makeImportantEvent="makeImportant($event)"
+        v-on:deleteTodoEvent="deleteTodo($event)"
+        v-on:todoDoneEvent="todoCompleted($event)"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
+import AddTodo from "./components/AddTodo.vue";
+import Todo from "./components/Todo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      addTodo: false,
+      todos: [
+        {
+          id: 6,
+          name: "Playing Cricket",
+          completed: true,
+          important: false,
+        },
+        {
+          id: 5,
+          name: "Making a React application",
+          completed: true,
+          important: false,
+        },
+        {
+          id: 4,
+          name: "Reading for the exams",
+          completed: false,
+          important: true,
+        },
+        {
+          id: 3,
+          name: "Android app project",
+          completed: true,
+          important: false,
+        },
+        {
+          id: 2,
+          name: "Attend RCF DevTeam meeting",
+          completed: false,
+          important: true,
+        },
+        {
+          id: 1,
+          name: "Making a Vue application",
+          completed: true,
+          important: false,
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Home,
+    AddTodo,
+    Todo,
+  },
+  methods: {
+    toogleAddTodo() {
+      this.addTodo = !this.addTodo;
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodoMethod(input) {
+      this.todos.unshift({
+        id: this.todos.length + 1,
+        name: input,
+        completed: false,
+        important: false,
+      });
+    },
+    makeImportant(id) {
+      this.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.important = !todo.important;
+        }
+      });
+    },
+    todoCompleted(id) {
+      this.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background: #42b983;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #101e2c;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.fa-plus {
+  color: #fff;
+  border: 2px solid #42b983;
+  cursor: pointer;
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card" v-bind:class="[data.important ? 'bgImp' : '']">
+    <i
+      @click="importantTodo(data.id)"
+      v-bind:class="[data.important ? 'important' : '']"
+      class="fas fa-crown"
+    ></i>
+    <h3 v-bind:class="[data.completed ? 'through' : '']">
+      {{ data.name }}
+      <i
+        v-bind:class="[data.completed ? 'done' : '']"
+        class="fas fa-check-square"
+        @click="doneTodo(data.id)"
+      ></i>
+    </h3>
+    <i @click="deleteTodo(data.id)" class="fas fa-trash-alt"></i>
+  </div>
+</template>
+<script>
+export default {
+  name: "Todo",
+
+  props: {
+    data: Object,
+  },
+  methods: {
+    deleteTodo(id) {
+      this.$emit("deleteTodoEvent", id);
+    },
+    importantTodo(id) {
+      this.$emit("makeImportantEvent", id);
+    },
+    doneTodo(id) {
+      this.$emit("todoDoneEvent", id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.through {
+  text-decoration: line-through;
+  font-weight: 200;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  height: 140px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.5);
+  background: #fff;
+  color: #101e2c;
+}
+.bgImp {
+  background: rgb(202, 210, 255);
+}
+.fa-crown {
+  color: rgba(39, 38, 1, 0.349);
+}
+.fa-trash-alt {
+  cursor: pointer;
+  color: rgb(160, 1, 1);
+  font-size: 1.3rem;
+}
+.fa-crown.important {
+  color: rgb(228, 224, 23);
+}
+.fa-check-square {
+  color: #101e2c31;
+  font-size: 1.5rem;
+}
+.fa-check-square.done {
+  color: #003a74;
+}
+</style>
